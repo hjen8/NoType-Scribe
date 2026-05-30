a60a6bb45ca17d8ab1abd2ec3376775212715da2
Huai-Jen <hjen86@Gmail.com>
2026-05-30 11:35:44 +0800

    Feature: 單句無句號微調與 BAT 自動提權防護

--- start_admin.bat
+++ start_admin.bat
@@ -1,2 +1,14 @@
 @echo off
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    goto :run
+) else (
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+)
+
+:run
 start "" "e:\AI_Work\NoType\skills\typeless-scribe\venv\Scripts\pythonw.exe" "e:\AI_Work\NoType\skills\typeless-scribe\main.py"
